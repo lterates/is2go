6a80310cadc3df1b6e31d72dfa84409dc9fa2aa7
lterates <9180199@esmad.ipp.pt>
2020-02-04 04:13:23 +0000

    Área de Administração - Adicionar novo Prato

--- src/views/Office.vue
+++ src/views/Office.vue
@@ -1,12 +1,17 @@
 <template>
     <v-app id="office">
-        <v-card style="margin-left: 20%; margin-top:8% ;max-width: 58%; height:40%" >
-            <v-card class="mb-12" flat color="white" height="320px" style="font-family: lato;">
+        <div style="margin-top:3%; margin-left: 39%">
+            <v-card-title style="font-family:lato; font-weight: 700" class="text-center">
+                Bem-vindo(a) à área de Administração!
+            </v-card-title>
+        </div>
+        <v-card style="margin-left: 20%; margin-top:3% ;max-width: 58%; height:40%" >
+            <v-card class="mb-12" flat color="white" height="340px" style="font-family: lato;">
                 <v-row>
                     <v-col class="text-center">
                         <v-btn depressed color="white" height="170px" width="99%" x-large>
                             <v-icon x-large>mdi-account-check</v-icon>
-                            <v-card-subtitle>Verificar Contas</v-card-subtitle>
+                            <v-card-subtitle>Gerir Contas</v-card-subtitle>
                         </v-btn>
                     </v-col>
                     <v-divider vertical></v-divider>
@@ -28,8 +33,8 @@
                 <v-row>
                     <v-col class="text-center">
                         <v-btn depressed color="white" height="170px" width="99%" x-large>
-                            <v-icon x-large>mdi-account-check</v-icon>
-                            <v-card-subtitle>Verificar Contas</v-card-subtitle>
+                            <v-icon x-large>mdi-plus-circle</v-icon>
+                            <v-card-subtitle>Adicionar Pratos</v-card-subtitle>
                         </v-btn>
                     </v-col>
                     <v-divider vertical></v-divider>
@@ -48,25 +53,72 @@
                     </v-col>
                 </v-row>
             </v-card>
-            
+        </v-card>
+        <v-card v-if="addDish = true" raised color="white" height="auto" max-width="58%" style="font-family: lato; margin-top:3%; margin-left: 20%" class="text-center">
+            <v-form>
+                <v-card-subtitle style="font-weight: 500; font-size: 22px">Novo Prato</v-card-subtitle>
+                <v-text-field class="mx-auto" style="max-width:80%;"
+                    outlined
+                    color="yellow darken-1"
+                    label="Nome"
+                    required
+                    v-model="dishName"
+                ></v-text-field>
+                <v-text-field class="mx-auto" style="max-width:80%;"
+                    color="yellow darken-1"
+                    outlined
+                    label="Tags"
+                    required
+                    v-model="dishTags"
+                ></v-text-field>
+                <v-text-field class="mx-auto" style="max-width:80%;"
+                    color="yellow darken-1"
+                    outlined
+                    label="Descrição"
+                    required
+                    v-model="dishDesc"
+                ></v-text-field>  
+                <v-file-input class="mx-auto" accept="image/*" prepend-icon="mdi-image-outline" style="max-width: 80%" color="yellow darken-1" label="Imagem do Prato"></v-file-input>
+                <v-btn color="#FFE138" class="mb-6 mt-4" style="font-weight: 700; font-family:lato" @click="submit">Adicionar Novo Prato</v-btn>
+            </v-form>
         </v-card>
         <!-- Botão de Cancelar o Pedido -->
-            <v-dialog v-model="dialog" width="300">
-                <template v-slot:activator="{ on }">
-                <v-btn class="mb-2" outlined style="margin-left: 41%;font-weight: 500; font-family:lato; margin-top: 3%" color="black" max-width="300px"
-                    v-on="on">Voltar à página de Início</v-btn>
-                </template>
-                <v-card>
-                <v-card-title style="font-family:lato; font-size: 18px" primary-title>
-                    Sair da zona de Administrador?
-                </v-card-title>
-                <v-divider></v-divider>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <router-link to="/"><v-btn text>Sim</v-btn></router-link>
-                    <v-btn text @click="dialog = false">Não</v-btn>
-                </v-card-actions>
-                </v-card>
-            </v-dialog>
+        <v-dialog v-model="dialog" width="300">
+            <template v-slot:activator="{ on }">
+            <v-btn class="mb-2" outlined style="margin-left: 41%;font-weight: 500; font-family:lato; margin-top: 3%" color="black" max-width="300px"
+                v-on="on">Voltar à página de Início</v-btn>
+            </template>
+            <v-card>
+            <v-card-title style="font-family:lato; font-size: 18px" primary-title>
+                Sair da zona de Administrador?
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <router-link to="/"><v-btn text>Sim</v-btn></router-link>
+                <v-btn text @click="dialog = false">Não</v-btn>
+            </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-app>
 </template>
+
+<script>
+export default {
+    name: 'Office',
+    counter: 0,
+    addDish: false,
+    data() {
+        return {
+            dialog: false,
+            dishName: '',
+            dishTags: '',
+            dishDesc: '',
+            dishImg: '',
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
